ec80004d6c50142e8ca0ad42bda104ed44663a18
coloro <colorolabs@gmail.com>
2018-07-10 14:10:18 -0400

    search farmaco

--- api/Farmacos.js
+++ api/Farmacos.js
@@ -3,6 +3,7 @@ const logger = require('../utils/Logger')
 const db = require('../db/Postgresql')
 
 const GET_FARMACO = 'SELECT codigo, dci, forma_farmaceutica, concentracion_valor, concentracion_unidad, es_restringido FROM farmacos WHERE codigo = $1'
+const SEARCH_FARMACO = "SELECT codigo, dci FROM farmacos WHERE dci LIKE '%' || $1 || '%'"
 
 function getFarmaco(req, res) {
   let codigo = parseInt(req.params.codigo, 10)
@@ -16,10 +17,31 @@ function getFarmaco(req, res) {
     if (result.rows.length === 0) {
       return fail(res, 'Farmaco not found', 404)
     }
+    logger.info('[Farmacos.searchFarmaco] Farmaco encontrado.')
     success(res, result.rows[0])
+  }).catch(e => {
+    logger.error({code: e.code, detail: e.detail, message: '[Farmacos.getFarmaco] Error en query GET_FARMACO'})
+    error(res, 'Internal error')
+  })
+}
+
+function searchFarmaco(req, res) {
+  let filter = req.query.q
+  logger.info('[Farmacos.searchFarmaco]' + filter)
+
+  if (!filter || filter.length === 0) {
+    return fail(res, 'Parameter missing or invalid')
+  }
+
+  db.query(SEARCH_FARMACO, [filter.trim()]).then(result => {
+    logger.info('[Farmacos.searchFarmaco] encontrados ' + result.rows.length)
+    success(res, result.rows)
+  }).catch(e => {
+    logger.error({code: e.code, detail: e.detail, message: '[Farmacos.searchFarmaco] Error en query SEARCH_FARMACO'})
+    error(res, 'Internal error')
   })
 }
 
 module.exports = {
-  getFarmaco
+  getFarmaco, searchFarmaco
 }
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const package = require('./package.json')
 const logger = require('./utils/Logger')
 
-const {getFarmaco} = require('./api/Farmacos')
+const {getFarmaco, searchFarmaco} = require('./api/Farmacos')
 const app = express()
 
 app.use(cors())
@@ -13,6 +13,7 @@ app.get('/', (req, res) => res.send({
 }))
 
 app.get('/farmaco/:codigo', getFarmaco)
+app.get('/farmaco', searchFarmaco)
 
 var port = process.env.PORT || 4000
 //app.listen(port, () => logger.info(package.name + '-' + package.version +  ' listening on port ' + port))
